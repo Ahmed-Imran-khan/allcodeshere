fc4e4cd1ff0493dc291367a6b989ac06c0766543
Ahmed Imran Khan <103501771+Ahmed-Imran-khan@users.noreply.github.com>
2023-04-01 02:27:03 +0500

    Add files via upload

---
+++ tictak.js
@@ -0,0 +1,83 @@
+var turn = 0;
+
+function changeText1() {
+    var button = document.getElementById("firstbox1");
+    if (turn % 2 == 0) {
+        button.innerHTML = "X";
+    } else {
+        button.innerHTML = "o";
+    }
+    turn++;
+}
+function changeText2() {
+    var button = document.getElementById("firstbox2");
+    if (turn % 2 == 0) {
+        button.innerHTML = "X";
+    } else {
+        button.innerHTML = "o";
+    }
+    turn++;
+}
+function changeText3() {
+    var button = document.getElementById("firstbox3");
+    if (turn % 2 == 0) {
+        button.innerHTML = "X";
+    } else {
+        button.innerHTML = "o";
+    }
+    turn++;
+}
+function changeText4() {
+    var button = document.getElementById("firstbox4");
+    if (turn % 2 == 0) {
+        button.innerHTML = "X";
+    } else {
+        button.innerHTML = "o";
+    }
+    turn++;
+}
+function changeText5() {
+    var button = document.getElementById("firstbox5");
+    if (turn % 2 == 0) {
+        button.innerHTML = "X";
+    } else {
+        button.innerHTML = "o";
+    }
+    turn++;
+}
+function changeText6() {
+    var button = document.getElementById("firstbox6");
+    if (turn % 2 == 0) {
+        button.innerHTML = "X";
+    } else {
+        button.innerHTML = "o";
+    }
+    turn++;
+}
+function changeText7() {
+    var button = document.getElementById("firstbox7");
+    if (turn % 2 == 0) {
+        button.innerHTML = "X";
+    } else {
+        button.innerHTML = "o";
+    }
+    turn++;
+}
+function changeText8() {
+    var button = document.getElementById("firstbox8");
+    if (turn % 2 == 0) {
+        button.innerHTML = "X";
+    } else {
+        button.innerHTML = "o";
+    }
+    turn++;
+}
+function changeText9() {
+    var button = document.getElementById("firstbox9");
+    if (turn % 2 == 0) {
+        button.innerHTML = "X";
+    } else {
+        button.innerHTML = "o";
+    }
+    turn++;
+}
